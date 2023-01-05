75bd3b9168a6ebda54e63c7ad5c9ba77c26f1267
Dijana <vatresd99@gmail.com>
2023-01-05 11:45:34 +0100

    Upis podataka u datoteke pri registraciji(u zavisnosti od tipa naloga)

--- hotelOSI/RegistrationForm.h
+++ hotelOSI/RegistrationForm.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
-
+#include <fstream>
+#include <string>
 namespace hotelOSI {
 
 	using namespace System;
@@ -51,6 +52,10 @@ namespace hotelOSI {
 	private: System::Windows::Forms::ListBox^ listBox1;
 	private: System::Windows::Forms::TextBox^ textBox6;
 	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::Label^ label9;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::CheckBox^ checkBox2;
+	private: System::Windows::Forms::CheckBox^ checkBox3;
 
 	private:
 		/// <summary>
@@ -81,18 +86,23 @@ namespace hotelOSI {
 			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
 			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
 			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(519, 588);
+			this->button1->Location = System::Drawing::Point(519, 735);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(102, 36);
 			this->button1->TabIndex = 11;
 			this->button1->Text = L"Potvrda";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &RegistrationForm::button1_Click);
 			// 
 			// textBox2
 			// 
@@ -100,6 +110,7 @@ namespace hotelOSI {
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(344, 26);
 			this->textBox2->TabIndex = 10;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &RegistrationForm::textBox2_TextChanged);
 			// 
 			// textBox1
 			// 
@@ -107,6 +118,7 @@ namespace hotelOSI {
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(344, 26);
 			this->textBox1->TabIndex = 9;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &RegistrationForm::textBox1_TextChanged);
 			// 
 			// label3
 			// 
@@ -148,6 +160,7 @@ namespace hotelOSI {
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(344, 26);
 			this->textBox3->TabIndex = 15;
+			this->textBox3->UseSystemPasswordChar = true;
 			// 
 			// textBox4
 			// 
@@ -184,6 +197,7 @@ namespace hotelOSI {
 			this->textBox5->Name = L"textBox5";
 			this->textBox5->Size = System::Drawing::Size(344, 26);
 			this->textBox5->TabIndex = 17;
+			this->textBox5->UseSystemPasswordChar = true;
 			// 
 			// label6
 			// 
@@ -239,11 +253,57 @@ namespace hotelOSI {
 			this->label8->TabIndex = 20;
 			this->label8->Text = L"Odgovor";
 			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label9->Location = System::Drawing::Point(7, 620);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(135, 30);
+			this->label9->TabIndex = 22;
+			this->label9->Text = L"Tip naloga";
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(277, 625);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(186, 24);
+			this->checkBox1->TabIndex = 23;
+			this->checkBox1->Text = L"Administrator resursa";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &RegistrationForm::checkBox1_CheckedChanged);
+			// 
+			// checkBox2
+			// 
+			this->checkBox2->AutoSize = true;
+			this->checkBox2->Location = System::Drawing::Point(277, 655);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(202, 24);
+			this->checkBox2->TabIndex = 24;
+			this->checkBox2->Text = L"Sisitemski administrator";
+			this->checkBox2->UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this->checkBox3->AutoSize = true;
+			this->checkBox3->Location = System::Drawing::Point(277, 685);
+			this->checkBox3->Name = L"checkBox3";
+			this->checkBox3->Size = System::Drawing::Size(77, 24);
+			this->checkBox3->TabIndex = 25;
+			this->checkBox3->Text = L"Klijent";
+			this->checkBox3->UseVisualStyleBackColor = true;
+			// 
 			// RegistrationForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(715, 645);
+			this->ClientSize = System::Drawing::Size(715, 783);
+			this->Controls->Add(this->checkBox3);
+			this->Controls->Add(this->checkBox2);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->label9);
 			this->Controls->Add(this->textBox6);
 			this->Controls->Add(this->label8);
 			this->Controls->Add(this->listBox1);
@@ -274,5 +334,83 @@ namespace hotelOSI {
 	}
 private: System::Void label7_Click(System::Object^ sender, System::EventArgs^ e) {
 }
+private: System::Void checkBox1_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	if (checkBox1->Checked==true)
+	{
+		checkBox2->Checked = false;
+		checkBox3->Checked = false;
+	}
+	else if (checkBox2->Checked == true)
+	{
+		checkBox1->Checked = false;
+		checkBox3->Checked = false;
+	}
+	else
+	{
+		checkBox1->Checked = false;
+		checkBox2->Checked = false;
+	}
+}
+	   
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		if (textBox1->Text != "" && textBox2->Text != "" && textBox3->Text != "" && textBox4->Text != "" && textBox5->Text != "" && textBox6->Text != "" && (
+			(checkBox1->Checked && checkBox2->Checked == false && checkBox3->Checked == false) ||
+			(checkBox2->Checked && checkBox1->Checked == false && checkBox3->Checked == false) ||
+			(checkBox3->Checked && checkBox2->Checked == false && checkBox1->Checked == false)
+			))
+	{
+		button1->Enabled = true;
+		MessageBox::Show("Uspješno ste se registovali!", "Registracija", MessageBoxButtons::OK, MessageBoxIcon::Information);
+		FILE* fp;
+		if ((checkBox1->Checked && checkBox2->Checked == false && checkBox3->Checked == false))
+		{
+			if (fp = fopen("Administratori resursa.txt", "a"))
+			{
+				MessageBox::Show("Uspjesan upis!", "Upis", MessageBoxButtons::OK, MessageBoxIcon::Information);
+				fprintf(fp, "%s %s %s %s %s %s", textBox1->Text, textBox2->Text, textBox3->Text, textBox4->Text, textBox5->Text, textBox6->Text);
+				fclose(fp);
+			}
+		}
+		else if((checkBox2->Checked && checkBox1->Checked == false && checkBox3->Checked == false))
+		{
+			if (fp = fopen("Sistemski administrator.txt", "a"))
+			{
+				MessageBox::Show("Uspjesan upis!", "Upis", MessageBoxButtons::OK, MessageBoxIcon::Information);
+				fprintf(fp, "%s %s %s %s %s %s", textBox1->Text, textBox2->Text, textBox3->Text, textBox4->Text, textBox5->Text, textBox6->Text);
+				fclose(fp);
+			}
+		}
+		else if ((checkBox3->Checked && checkBox2->Checked == false && checkBox1->Checked == false))
+		{
+			if (fp = fopen("Klijenti.txt", "a"))
+			{
+				MessageBox::Show("Uspjesan upis!", "Upis", MessageBoxButtons::OK, MessageBoxIcon::Information);
+				fprintf(fp, "%s %s %s %s %s %s", textBox1->Text, textBox2->Text, textBox3->Text, textBox4->Text, textBox5->Text, textBox6->Text);
+				fclose(fp);
+			}
+
+		}
+		
+
+	}
+	else {
+		button1->Enabled = false;
+		MessageBox::Show("Neuspješna registracija!", "Registracija", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		this->Hide();
+		RegistrationForm^ form2 = gcnew RegistrationForm;
+		form2->Show();
+
+	}
+
+
+}
+	   
+
+
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+
 };
 }
